3cdd799040a430f371713ae3b2812903926fd680
mapogolions <ivanovpavelalex45@gmail.com>
2020-10-25 21:46:20 +0500

    in memory config

---
+++ InMemoryConfiguration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
+
+namespace InMemoryConfiguration
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            var configRoot = new ConfigurationBuilder()
+                .Add(new MemoryConfigurationSource // will be checked last
+                {
+                    InitialData = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("env", "Development"),
+                        new KeyValuePair<string, string>("host", "localhost"),
+                        new KeyValuePair<string, string>("port", "9000"),
+                    }
+                })
+                .AddInMemoryCollection(new List<KeyValuePair<string, string>> // will be checked first
+                {
+                    new KeyValuePair<string, string>("env", "Production")
+                })
+                .Build();
+
+            Console.WriteLine($"{configRoot["env"]}|{configRoot["host"]}|{configRoot["port"]}");
+        }
+    }
+}
