5decbe5f258b71ad985c0664104c4a1f612f5b3f
mapogolions <ivanovpavelalex45@gmail.com>
2020-10-26 14:20:07 +0500

    add user secrets configuration

---
+++ UserSecretConfiguration/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿using Microsoft.Extensions.Configuration.UserSecrets;
+
+[assembly: UserSecretsId("5ACA56FF-B3F1-4C66-9E70-E93FAA4E7180")]
---
+++ UserSecretConfiguration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace UserSecretConfiguration
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            var configRoot = new ConfigurationBuilder()
+                .AddUserSecrets(Assembly.GetExecutingAssembly())
+                .Build();
+
+            Console.WriteLine($"{configRoot["env"]}|{configRoot["host"]}|{configRoot["port"]}");
+        }
+    }
+}
