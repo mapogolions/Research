b4557bf1e0ff4857c6a43e58540d3ef4c3f1f2d1
mapogolions <ivanovpavelalex45@gmail.com>
2020-10-26 22:58:06 +0500

    fix

--- UserSecretConfiguration/GenerateUserSecretId.ps1
+++ UserSecretConfiguration/GenerateUserSecretId.ps1
@@ -1,4 +1,9 @@
-Set-Location $env:APPDATA/Microsoft/UserSecrets
+Set-Location $env:APPDATA/Microsoft
+if( -Not (Test-Path -Path ./UserSecrets ) )
+{
+    New-Item -ItemType directory -Path ./UserSecrets
+}
+Set-Location ./UserSecrets
 $user_secret_id = [guid]::NewGuid().Guid
 New-Item $user_secret_id -ItemType Directory
 New-Item $user_secret_id/secrets.json
