50d1683321e7ac816f8e93aaad1a4d9d924c9dfa
mapogolions <41992686+mapogolions@users.noreply.github.com>
2020-10-26 21:55:37 +0500

    Fix typo

--- UserSecretConfiguration/GenerateUserSecretId.ps1
+++ UserSecretConfiguration/GenerateUserSecretId.ps1
@@ -1,5 +1,5 @@
 Set-Location $env:APPDATA/Microsoft/UserSecrets
-$user_sercret_id = [guid]::NewGuid().Guid
-New-Item $user_sercret_id -ItemType Directory
-New-Item $user_sercret_id/secrets.json
-'{"host": "localhost", "port": 9000, "env": "Development"}' | Add-Content $user_sercret_id/secrets.json
+$user_secret_id = [guid]::NewGuid().Guid
+New-Item $user_secret_id -ItemType Directory
+New-Item $user_secret_id/secrets.json
+'{"host": "localhost", "port": 9000, "env": "Development"}' | Add-Content $user_secret_id/secrets.json
