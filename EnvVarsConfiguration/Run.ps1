a470806451c74a7125622d60da70aa9fea5f773f
mapogolions <ivanovpavelalex45@gmail.com>
2020-10-31 19:30:45 +0500

    improve powershell script

--- EnvVarsConfiguration/Run.ps1
+++ EnvVarsConfiguration/Run.ps1
@@ -1,4 +1,4 @@
 $env:TempEnv="Development"
-$env:TempDevelopment__Host="localhost"
-$env:TempDevelopment__Port=8000
+New-Item -Name "Temp${env:TempEnv}__Host" -Value locahost -ItemType Variable -Path Env:
+New-Item -Name "Temp${env:TempEnv}__Port" -Value 9000 -ItemType Variable -Path Env:
 dotnet run ./
