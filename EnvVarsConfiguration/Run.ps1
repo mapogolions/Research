20f359caf845b666eff260c9f95c7fa86e2485cb
Pavel Ivanov <ivanovpavelalex45@gmail.com>
2020-10-31 19:30:55 +0500

    fix typo

--- EnvVarsConfiguration/Run.ps1
+++ EnvVarsConfiguration/Run.ps1
@@ -1,4 +1,4 @@
 $env:TempEnv="Development"
-New-Item -Name "Temp${env:TempEnv}__Host" -Value locahost -ItemType Variable -Path Env:
+New-Item -Name "Temp${env:TempEnv}__Host" -Value localhost -ItemType Variable -Path Env:
 New-Item -Name "Temp${env:TempEnv}__Port" -Value 9000 -ItemType Variable -Path Env:
 dotnet run ./
