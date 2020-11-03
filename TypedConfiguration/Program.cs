f8203a4eaff87dbda7aa7d284dfa82fecd47a114
mapogolions <ivanovpavelalex45@gmail.com>
2020-11-03 21:18:14 +0500

    add typed configuration

---
+++ TypedConfiguration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypedConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
